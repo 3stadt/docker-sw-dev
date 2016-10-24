d6204825c2ad708c98b4f46c0964acff3ed19fcd
Niklas Dzösch <nd@shopware.com>
2016-10-24 10:11:10 +0200

    bugfixes, added minimal BU config

--- elasticsearch/config/scripts/gitkeep.groovy
+++ elasticsearch/config/scripts/gitkeep.groovy
@@ -1,13 +1,1 @@
-GET /_search
-{
-  "script_fields": {
-    "my_field": {
-      "script": {
-        "inline": "1 + my_var",
-        "params": {
-          "my_var": 2
-        }
-      }
-    }
-  }
-}
+1 + my_var
